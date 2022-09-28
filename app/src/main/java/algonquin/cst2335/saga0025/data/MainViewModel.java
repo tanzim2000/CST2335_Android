806865c8ab798b0c95f9fa74e952335e90dd9f2e
tanzim2000 <tanzimahmedsagar@gmail.com>
2022-09-28 17:59:25 -0400

    Finished week 2

--- app/src/main/java/algonquin/cst2335/saga0025/data/MainViewModel.java
+++ app/src/main/java/algonquin/cst2335/saga0025/data/MainViewModel.java
@@ -1,8 +1,10 @@
 package algonquin.cst2335.saga0025.data;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MainViewModel extends ViewModel {
     public MutableLiveData<String> editString = new MutableLiveData<>();
+    public MutableLiveData<Boolean> isSelected = new MutableLiveData<>();
 }
--- app/src/main/java/algonquin/cst2335/saga0025/ui/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/saga0025/ui/MainActivity.java
@@ -2,8 +2,8 @@ package algonquin.cst2335.saga0025.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
-
 import android.os.Bundle;
+import android.widget.Toast;
 
 import algonquin.cst2335.saga0025.data.MainViewModel;
 import algonquin.cst2335.saga0025.databinding.ActivityMainBinding;
@@ -17,13 +17,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         model = new ViewModelProvider(this).get(MainViewModel.class);
         variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
+
         setContentView(variableBinding.getRoot());
-        variableBinding.textView.setText((CharSequence) model.editString);
-        variableBinding.myButton.setOnClickListener(click -> {
-            model.editString.postValue(variableBinding.textView.getText().toString());
+        variableBinding.textView.setText(model.editString.toString());
+        variableBinding.myButton.setOnClickListener(click -> model.editString.postValue(variableBinding.textView.getText().toString()));
+        model.editString.observe(this, s -> variableBinding.textView.setText("Your edit text has "+ s));
+        model.isSelected.observe(this, selected -> {
+            variableBinding.checkBox.setChecked(selected);
+            variableBinding.radioButton.setChecked(selected);
+            variableBinding.switch1.setChecked(selected);
+            Toast.makeText(this,"Now the value is: " + selected, Toast.LENGTH_SHORT).show();
         });
-        model.editString.observe(this, s -> {
-            variableBinding.textView.setText("Your edit text has "+ s);
-        });
+        variableBinding.checkBox.setOnCheckedChangeListener( (btn, isSelected) -> model.isSelected.postValue(isSelected));
+        variableBinding.switch1.setOnCheckedChangeListener( (btn, isSelected) -> model.isSelected.postValue(isSelected));
+        variableBinding.radioButton.setOnCheckedChangeListener( (btn, isSelected) -> model.isSelected.postValue(isSelected));
+        variableBinding.myImageButton.setOnClickListener(click -> Toast.makeText(this,"The width = " + click.getWidth() + " and height = " + click.getHeight(), Toast.LENGTH_SHORT).show());
     }
 }
